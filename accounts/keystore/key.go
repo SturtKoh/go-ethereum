1069c39dedcad0d420b4868b60f5521368ab3ee9
StuartKoh <34223984+SturtKoh@users.noreply.github.com>
2018-10-16 15:03:29 +0900

    generate key and import chg
    generate key and import chg to own go-eth

--- accounts/keystore/key.go
+++ accounts/keystore/key.go
@@ -29,9 +29,9 @@ import (
 	"strings"
 	"time"
 
-	"github.com/ethereum/go-ethereum/accounts"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/SturtKoh/go-ethereum/accounts"
+	"github.com/SturtKoh/go-ethereum/common"
+	"github.com/SturtKoh/go-ethereum/crypto"
 	"github.com/pborman/uuid"
 )
 
